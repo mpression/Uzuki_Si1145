67c3a4aeaae2acd97dccdc7e360491189734c0a8
Kenji Ohno <kenji.ohno@mac.com>
2014-10-24 08:48:57 +0900

    Master

---
+++ Uzuki_Si1145.ino
@@ -0,0 +1,83 @@
+/**************************************************************************
+Copyright (c) 2014, Macnica Inc.,
+All rights reserved.
+
+This is a library for the Si1145 UV/IR/Visible Light Sensor on Uzuki IoT Sensor Shield
+http://www.m-pression.com/ja/solutions/boards/uzuki-shield
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies, 
+either expressed or implied, of the FreeBSD Project.
+******************************************************************************/
+
+/*************************************************** 
+  This is a library for the Si1145 UV/IR/Visible Light Sensor
+
+  Designed specifically to work with the Si1145 sensor in the
+  adafruit shop
+  ----> https://www.adafruit.com/products/1777
+
+  These sensors use I2C to communicate, 2 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <Wire.h>
+#include "Adafruit_SI1145.h"
+
+Adafruit_SI1145 uv = Adafruit_SI1145();
+
+void setup() {
+  Serial.begin(9600);
+  
+  Serial.println("Adafruit SI1145 test");
+  
+  if (! uv.begin()) {
+    Serial.println("Didn't find Si1145");
+    while (1);
+  }
+
+  Serial.println("OK!");
+}
+
+void loop() {
+  Serial.println("===================");
+  Serial.print("Vis: "); Serial.println(uv.readVisible());
+  Serial.print("IR: "); Serial.println(uv.readIR());
+  
+  // Uncomment if you have an IR LED attached to LED pin!
+  //Serial.print("Prox: "); Serial.println(uv.readProx());
+
+  float UVindex = uv.readUV();
+  // the index is multiplied by 100 so to get the
+  // integer index, divide by 100!
+  UVindex /= 100.0;  
+  Serial.print("UV: ");  Serial.println(UVindex);
+
+  delay(1000);
+}
